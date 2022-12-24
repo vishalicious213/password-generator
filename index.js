3986f0253776b27fd0506ce3ba764da98c06bc1e
vishalicious213 <vishalicious213@gmail.com>
2022-12-23 21:38:19 -0800

    added functions to generate passwords and connected them to button's onclick. passwords are being generated and rendered

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
-const pw1 = document.getElementById("pw1")
-const pw2 = document.getElementById("pw2")
+let pw1 = document.getElementById("pw1")
+let pw2 = document.getElementById("pw2")
+
+function generatePasswords() {
+    let index = 0
+    let password = ""
+
+    for (let i=0; i < 15; i++) {
+        index = Math.floor(Math.random() * characters.length)
+        password += characters[index]
+    }
+
+    return password
+}
 
 function generate() {
-    pw1.textContent = "12345"
-    pw2.textContent = "67890"
+    pw1.textContent = generatePasswords()
+    pw2.textContent = generatePasswords()
 }
