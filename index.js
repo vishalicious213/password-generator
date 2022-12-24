02104b71035103829e78f202c829addf1d886571
vishalicious213 <vishalicious213@gmail.com>
2022-12-23 21:11:26 -0800

    drafted generate() function with hard-coded values. moved script tag for index.js to end of html file, so its read AFTER the pw fields exist in the DOM

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Password Generator")
+const pw1 = document.getElementById("pw1")
+const pw2 = document.getElementById("pw2")
+
+function generate() {
+    pw1.textContent = "12345"
+    pw2.textContent = "67890"
+}
