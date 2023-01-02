5b05fe5b3cb4d9b1540ea18fa8a5b05fffaa9603
vishalicious213 <vishalicious213@gmail.com>
2023-01-02 13:28:07 -0800

    reformatted character set checkboxes and styled labels

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const checkLowercase = document.getElementById("lowercase")
 const checkNumbers = document.getElementById("numbers")
 const checkSymbols = document.getElementById("symbols")
 
-// const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
-
 let pw1 = document.getElementById("pw1")
 let pw2 = document.getElementById("pw2")
 
