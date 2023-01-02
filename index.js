ba97df3ff0abd25894cb490fd8bf3d4f8e075952
vishalicious213 <vishalicious213@gmail.com>
2023-01-02 11:45:41 -0800

    options addEventListener sees input from each checkbox

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const chars_lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 const chars_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const chars_symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
-// const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+const options = document.getElementById("options")
+const checkUppercase = document.getElementById("uppercase")
+const checkLowercase = document.getElementById("lowercase")
+const checkNumbers = document.getElementById("numbers")
+const checkSymbols = document.getElementById("symbols")
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
 let pw1 = document.getElementById("pw1")
 let pw2 = document.getElementById("pw2")
+
+options.addEventListener("change", () => {
+    if (checkUppercase.checked) {
+        console.log("uppercase")
+    }
+    if (checkLowercase.checked) {
+        console.log("lowercase")
+    }
+    if (checkNumbers.checked) {
+        console.log("numbers")
+    }
+    if (checkSymbols.checked) {
+        console.log("symbols")
+    }
+})
 
 function generatePasswords() {
     let index = 0
